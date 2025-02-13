f5d7a7730b8f899f494e626d0980af7192af0b37
keshavlovescode <160206971+keshavlovescode@users.noreply.github.com>
2025-02-13 17:32:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.getElementById('toggleButton');
+
+  toggleButton.addEventListener('click', () => {
+    alert("Sending a virtual hug to the best Mom in the world! ❤️");
+  });
+});
